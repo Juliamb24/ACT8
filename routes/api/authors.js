4699a4795d1a36da53f79ca202a21845971811af
Julia MB <152322160+Juliamb24@users.noreply.github.com>
2024-06-04 00:56:43 +0200

    Update authors.js

--- routes/api/authors.js
+++ routes/api/authors.js
@@ -17,12 +17,7 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) =>{
     try{
         const [result] = await create(req.body);
-<<<<<<< HEAD
         res.status(201).json({ message: 'Autor añadido correctamente', result });
-=======
-        const message = "Autor añadido correctamente"
-        res.json({result, message});
->>>>>>> 19fc81e9f91b23a74c4b98806def4ce70fc4b067
         
     }catch(err){
         res.status(500).json({ error: err.message });
@@ -33,12 +28,7 @@ router.post('/', async (req, res) =>{
 router.put('/:authorId', async (req, res) =>{
     try{
         const [result] = await update(req.params.authorId, req.body);
-<<<<<<< HEAD
         res.json({ message: 'Autor modificado correctamente', result });
-=======
-        const message = "Autor modificado correctamente"
-        res.json({result, message});
->>>>>>> 19fc81e9f91b23a74c4b98806def4ce70fc4b067
     }catch(err){
         res.status(500).json({ error: err.message });
     }
@@ -49,12 +39,7 @@ router.put('/:authorId', async (req, res) =>{
 router.delete('/:authorId', async (req, res) =>{
     try{
         const [result] = await deleteById(req.params.authorId);
-<<<<<<< HEAD
         res.json({ message: 'Autor eliminado correctamente', result });
-=======
-        const message = "Autor eliminado correctamente"
-        res.json({result, message});
->>>>>>> 19fc81e9f91b23a74c4b98806def4ce70fc4b067
     }catch(err){
         res.status(500).json({ error: err.message });
     }
